eaac46f23c792b9663eeeb91d3614e1aa3e64e9b
Irina <106918584+IrinaGrekova@users.noreply.github.com>
2022-07-15 10:25:31 +0300

    Initial commit

---
+++ pr/block1.6final/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  // Входной файл
+  entry: [
+    './src/js/index.js'
+  ],
+
+  // Выходной файл
+  output: {
+    publicPath: '',
+    filename: './js/bundle.js'
+  },
+
+  // Source maps для удобства отладки
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      // Транспилируем js с babel
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+
+      // Подключаем шрифты из css
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./fonts/[name].[ext]'
+          },
+        ]
+      },
+
+      // Подключаем картинки из css
+      {
+        test: /\.(svg|png|jpg|jpeg|webp)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./static/[name].[ext]'
+          },
+        ]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    // Подключаем файл html, стили и скрипты встроятся автоматически
+    new HtmlWebpackPlugin({
+      title: 'Webpack 5 Starter',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      }
+    }),
+
+    // Кладем стили в отдельный файлик
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+
+    // Копируем картинки
+    new CopyWebpackPlugin({
+      patterns: [
+      {
+        from: './src/img',
+        to: 'img',
+      },
+      ]
+    })
+  ],
+};
